36247e2526c9fc911113c7a489bc3b6f162daaa3
Vinicius de Moura <viniciusdemourarosa@gmail.com>
2026-02-22 23:09:22 -0300

    refactor: move functions order

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -21,15 +21,6 @@ const makeEmailValidatorWithError = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeFakeRequest = (): HttpRequest => ({
-  body: {
-    name: 'any_name',
-    email: 'any_email@mail.com',
-    password: 'any_password',
-    passwordConfirmation: 'any_password'
-  }
-})
-
 interface SutTypes {
   sut: SignUpController
   emailValidatorStub: EmailValidator
@@ -55,6 +46,15 @@ const makeAddAccount = (): AddAccount => {
   }
   return new AddAccountStub()
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
