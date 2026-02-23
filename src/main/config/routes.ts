eb9f05fb6611a13890d85c497804628ec94b633e
Vinicius de Moura <viniciusdemourarosa@gmail.com>
2026-02-22 22:20:06 -0300

    fix(routes): ensure routes are loaded synchronously to prevent 404 during test

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,7 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  fg.sync('**/src/main/routes/**routes.ts').forEach(file => {
+    require(`../../../${file}`).default(router)
+  })
 }
